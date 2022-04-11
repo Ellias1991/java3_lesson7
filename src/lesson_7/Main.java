d6a20e121521d191699a16439e0b4db278db833a
Илья <llya.tumanow2015@yandex.ru>
2022-04-12 00:01:37 +0300

    Initial commit

---
+++ src/lesson_7/Main.java
@@ -0,0 +1,8 @@
+package lesson_7;
+
+public class Main {
+
+    public static void main(String[] args) {
+	StartTest.start(MyTest.class);
+    }
+}
---
+++ src/lesson_7/MyTest.java
@@ -0,0 +1,33 @@
+package lesson_7;
+
+public class MyTest {
+
+    @BeforeSuite
+    public static void test2() {
+        System.out.println("test2 BeforeSuite");
+    }
+
+    @Test
+    public static void test4() {
+        System.out.println("test4");
+    }
+
+    @Test
+    public static void test1() {
+        System.out.println("test1");
+    }
+
+    @Test(priority = 3)
+    public static void test8() {
+        System.out.println("test8 priority = 3");
+    }
+
+    @Test(priority = 4)
+    public static void test9() {
+        System.out.println("test9 priority = 4");
+    }
+    @AfterSuite
+    public static void test5() {
+        System.out.println("test5 AfterSuite");
+    }
+}
